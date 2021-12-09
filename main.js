380ab70ad6538c3a72871e724224e628b37c1cdc
Aalia09 <76902204+Aalia09@users.noreply.github.com>
2021-12-09 19:35:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 statuss = "";
+objects = [];
 
 function preload()
 {
@@ -28,8 +29,35 @@ function modelLoaded()
     video.volume(0);
 
 }
+function gotResult(error , results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
 
 function draw()
 {
     image(video, 0 , 0 , 480 , 380);
+    if(statuss != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+
+            fill("#3d3ca6");
+
+            percent = floor(objects[i].confidence *100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("#3d3ca6")
+            rect(objects[i].x, objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
 }
